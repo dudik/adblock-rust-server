d0aac143cd79169f7eede3fc43c1c05171d7f23d
Samuel Dudik <samuel.dudik@gmail.com>
2021-03-11 19:20:33 +0100

    Add update mechanism

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@ use std::sync::Arc;
 use std::fs;
 use std::env::var;
 use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use adblock::engine::Engine;
 use adblock::lists::{FilterFormat, FilterSet};
+
+use attohttpc;
 
 fn handle_client(mut stream: UnixStream, blocker: Arc<Engine>) {
     let buf = BufReader::new(stream.try_clone().unwrap());
@@ -58,11 +61,62 @@ fn main() -> std::io::Result<()> {
     let home_dir = var("HOME").unwrap();
     let config_dir = home_dir.to_owned() + "/.config/ars";
     let lists_dir = config_dir.to_owned() + "/lists";
-    let engine_file = config_dir + "/engine";
+    let engine_file = config_dir.to_owned() + "/engine";
+    let urls_file = config_dir.to_owned() + "/urls";
 
+    let mut updated = false;
     let mut blocker;
 
-    if Path::new(&engine_file).exists() {
+    fs::create_dir_all(&lists_dir).unwrap();
+
+    if Path::new(&urls_file).exists() {
+        let mut file = fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .open(&urls_file)
+            .unwrap();
+        let reader = BufReader::new(file.try_clone().unwrap());
+        let timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+        let mut out = String::new();
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            let mut parts = line.split(' ');
+            let url = parts.next().unwrap();
+
+            if parts.clone().count() == 0 || parts.next().unwrap().parse::<u64>().unwrap() < timestamp.as_secs() {
+                // list needs to be updated
+                updated = true;
+                let filename = url.split('/').last().unwrap();
+                let mut res = attohttpc::get(&url).send().unwrap();
+                let f = fs::File::create(lists_dir.to_owned() + "/" + filename).unwrap();
+
+                let freader = BufReader::new(&mut res);
+                for fline in freader.lines() {
+                    let fline = fline.unwrap();
+                    if fline.contains("! Expires: ") {
+                        let days = fline.split(' ').nth(2).unwrap().parse::<u64>().unwrap() * 24 * 3600;
+                        let stamp = std::time::Duration::new(days, 0) + SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+                        out.push_str(&url);
+                        out.push(' ');
+                        out.push_str(&stamp.as_secs().to_string());
+                        break;
+                    }
+                }
+
+                res.write_to(f).unwrap();
+            } else {
+                out.push_str(&line);
+            }
+
+            out.push('\n');
+        }
+
+        file.seek(std::io::SeekFrom::Start(0)).unwrap();
+        file.write_all(&out.into_bytes()).unwrap();
+    }
+
+    if Path::new(&engine_file).exists() && !updated {
         blocker = Engine::new(true);
         let data = fs::read(engine_file).unwrap();
         blocker.deserialize(&data).unwrap();
